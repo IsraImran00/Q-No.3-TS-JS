e95ab79821ab4b008c2be33e297f0759177c342a
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-03-10 16:56:00 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,5 @@
+/*3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.*/
+var firstName = "Isra Imran";
+console.log(firstName.toLowerCase()); //lowercase
+console.log(firstName.toUpperCase()); //uppercase
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1)); //titlecase
